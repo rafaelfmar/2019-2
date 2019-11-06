3c302f27de14a656a7f975502bbee0b8c41cfb27
rafaelfmar <rafael.fmarques@outlook.com>
2019-11-06 19:41:27 -0300

    ClienteModel

---
+++ week-13/app/src/main/java/com/example/app/apresentacao/ClienteModel.java
@@ -0,0 +1,18 @@
+package com.example.app.apresentacao;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClienteModel {
+    private Long id;
+    private String nome;
+    private int idade;
+    private double limiteCredito;
+    private String telefone;
+}
